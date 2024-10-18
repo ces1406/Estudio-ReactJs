9ea7173b939bc71b2881322073bb040185c4f684
cesar-win10-pc <ces1406@gmail.com>
2024-10-18 12:04:17 -0300

    limpieza de seteo de Vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import CompoClase from './compoClass'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,9 +9,6 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
@@ -21,13 +18,14 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <CompoClase propA='soy una prop'>
+        <h3>children-1</h3>
+        <h3>children-2</h3>
+      </CompoClase>
     </>
   )
 }
